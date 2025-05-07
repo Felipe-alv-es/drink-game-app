49972783846c229dff34b263f3d3f6406d5ccc14
Felipe-alv-es <brago.sama@gmail.com>
2025-05-07 14:50:13 -0300

    chore: game and challenges lists improved

--- src/Assets/Arrays/DefaultList.tsx
+++ src/Assets/Arrays/DefaultList.tsx
@@ -6,36 +6,106 @@ export interface ListTypes {
 
 export const DesafioPadrão: ListTypes[] = [
   {
-    title: "Deixe alguem desenhar uma piroca na sua testa",
-    description: "E fique até o final do jogo",
+    title: "Beba como um cachorro",
+    description: "Sim, como um cachorro.",
+    quantity: 4,
+  },
+  {
+    title: "Conte com que frequência você se masturba",
+    description: "Pra que a vergonha? É natural.",
+    quantity: 6,
+  },
+  {
+    title: "Venda os olhos até o próximo turno",
+    description: 'Vai confiar nos seus "amigos"? Boa sorte.',
+    quantity: 4,
+  },
+  {
+    title: "Cite 8 apelidos para pênis",
+    description: "Se passar de 1 minuto, você bebe.",
+    quantity: 4,
+  },
+  {
+    title: "Dê uma cambalhota",
+    description: "Todo mundo já fez isso, né?",
+    quantity: 4,
+  },
+  {
+    title: "Tire uma peça de roupa",
+    description: "Não vale trapacear, hein!",
+    quantity: 4,
+  },
+  {
+    title: "Mumifique sua cabeça com papel higiênico",
+    description: "E fique assim por três rodadas.",
+    quantity: 6,
+  },
+  {
+    title: "Faça um boquete em uma garrafa",
+    description: "Não precisa fingir que não curte.",
     quantity: 8,
   },
   {
-    title: "Deixe alguem desenhar outra piroca na sua testa",
-    description: "E fique até o final do jogo",
-    quantity: 8,
+    title: "Deixe seus amigos refazerem seu penteado",
+    description: "Vai ficar maravilhoso... talvez.",
+    quantity: 4,
   },
   {
-    title: "Deixe alguem desenhar mais uma piroca na sua testa",
-    description: "E fique até o final do jogo",
+    title: "Deite no chão feito uma estrela do mar",
+    description: "E fique por 10 segundos.",
+    quantity: 2,
+  },
+  {
+    title: "Coloque ketchup na sua bebida",
+    description: "Se for asiático, pode ser shoyu.",
+    quantity: 6,
+  },
+  {
+    title: "Deixe alguém desenhar uma piroca na sua testa",
+    description: "E fique até o final do jogo.",
     quantity: 8,
   },
 ];
 
 export const ObedeçaOLider: ListTypes[] = [
   {
-    title: "Coma sabonete",
-    description: "Sim, é isso mesmo",
+    title: "Nova regra: Ninguém pode xingar por três rodadas",
+    description: "Beba toda vez que vacilar.",
     quantity: 4,
   },
   {
-    title: "Coma outro sabonete",
-    description: "Sim",
+    title:
+      "Nova regra: Todas as frases devem soar como perguntas por três rodadas",
+    description: "Toda vez que vacilar? Não aguento mais beber?",
     quantity: 4,
   },
   {
-    title: "Agora coma sabão em pedra",
-    description: "Gotoso",
+    title: "Nova regra: Se atire no chão quando alguém gritar “Granada”",
+    description: "Mais fácil continuar no chão.",
+    quantity: 8,
+  },
+  {
+    title: "Nova regra: Você só pode conversar cochichando por três rodadas",
+    description: "Beba toda vez que falhar.",
     quantity: 4,
+  },
+  {
+    title: "Nova regra: Você só pode se locomover em sua moto invisível",
+    description: "Cuidado com as câmeras.",
+    quantity: 2,
+  },
+];
+
+export const Constrangimento: ListTypes[] = [
+  {
+    title:
+      "Envie uma mensagem de voz dizendo tudo o que vai fazer com seu terceiro contato na cama",
+    description: "Grave com emoção e mande",
+    quantity: 8,
+  },
+  {
+    title: "Poste um status dizendo o que você gosta de fazer na hora H",
+    description: "Com detalhes, a galera precisa saber.",
+    quantity: 8,
   },
 ];
